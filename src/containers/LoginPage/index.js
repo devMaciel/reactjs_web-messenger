b1c749b5c1a21541587fff83b94e8a465a191bae
João Pedro Maciel <joomaciiel2@outlook.com>
2020-10-05 21:42:59 -0300

    Header component and Login initial form

--- src/containers/LoginPage/index.js
+++ src/containers/LoginPage/index.js
@@ -1,4 +1,7 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import Layout from '../../components/Layout'
+import Card from '../../components/Layout/UI/Card'
+import './style.css'
 
 /**
 * @author
@@ -6,8 +9,42 @@ import React from 'react'
 **/
 
 const LoginPage = (props) => {
+
+  //hooks
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+
   return(
-    <div>LoginPage</div>
+    <Layout>
+      <div className="loginContainer">
+        <Card>
+          <form action="">
+           
+            <input 
+              name="email"
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Email"
+            />
+
+            <input 
+              name="password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Password"
+            />
+
+            <div>
+              <button>Login</button>
+            </div>
+
+          </form>
+        </Card>
+      </div>
+    </Layout>
    )
 
  }

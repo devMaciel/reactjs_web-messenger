6bc0b24af16269a87d6de667da0823239f209228
João Pedro Maciel <joomaciiel2@outlook.com>
2020-10-08 20:56:47 -0300

    frontend home

--- src/containers/HomePage/index.js
+++ src/containers/HomePage/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import Layout from '../../components/Layout'
+// import Layout from '../../components/Layout'
+import './style.css';
 
 /**
 * @author
@@ -7,12 +8,36 @@ import Layout from '../../components/Layout'
 **/
 
 const HomePage = (props) => {
-  return(
-    <Layout>
-      <div>HomePage</div>
-    </Layout>
-   )
+  return (
+    <section className="container">
+    <div className="listOfUsers">
 
- }
+        <div className="displayName">
+            <div className="displayPic">
+                <img src="https://i.pinimg.com/originals/be/ac/96/beac96b8e13d2198fd4bb1d5ef56cdcf.jpg" alt="" />
+            </div>
+            <div style={{margin: '0 10px'}}>
+                <span style={{fontWeight: 500}}>Usuário 1</span>
+            </div>
+        </div>
+                
+    </div>
+    <div className="chatArea">
+        <div className="chatHeader"> Usuário 1 </div>
+        <div className="messageSections">
 
-export default HomePage
+            <div style={{ textAlign: 'left' }}>
+                <p className="messageStyle" >Hello User</p>
+            </div>
+
+        </div>
+        <div className="chatControls">
+            <textarea />
+            <button>Send</button>
+        </div>
+    </div>
+</section>
+  );
+}
+
+export default HomePage;

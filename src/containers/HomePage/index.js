00095f889ce499914d571798cdf9f011fca1c123
João Pedro Maciel <joomaciiel2@outlook.com>
2020-10-10 23:11:02 -0300

    chat started

--- src/containers/HomePage/index.js
+++ src/containers/HomePage/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { getRealtimeUsers } from '../../actions';
 import Layout from '../../components/Layout'
@@ -9,11 +9,33 @@ import './style.css';
 * @function HomePage
 **/
 
+const User = (props) => {
+
+    const {user, onClick} = props;
+
+    return (
+        <div onClick={() => onClick(user)} className="displayName">
+            <div className="displayPic">
+                <img src="https://i.pinimg.com/originals/be/ac/96/beac96b8e13d2198fd4bb1d5ef56cdcf.jpg" alt="" />
+            </div>
+            <div style={{ display:'flex', flex:'1', justifyContent:'space-between', margin: '0 10px'}}>
+                <span style={{fontWeight: 500}}>{user.firstName} {user.lastName}</span>
+                <span>{user.isOnline ? 'online' : 'offline'}</span>
+            </div>
+        </div>
+    );
+}
+
 const HomePage = (props) => {
 
   const dispatch = useDispatch();
   const auth = useSelector(state => state.auth);
   const user = useSelector(state => state.user);
+
+  const [chatStarted, setChatStarted] = useState(false);
+  const [chatUser, setChatUser] = useState('');
+
+
   let unsubscribe;
 
   useEffect(() => {
@@ -38,7 +60,13 @@ const HomePage = (props) => {
   }, []);
 
 
-//   console.log(user);
+  const initChat = (user) => {
+    setChatStarted(true);
+    setChatUser(`${user.firstName} ${user.lastName}`);
+    //console.log(user);
+  }
+
+
 
   return (
     <Layout>
@@ -48,32 +76,41 @@ const HomePage = (props) => {
               user.users.length > 0 ?
               user.users.map(user => {
                   return(
-                    <div key={user.uid} className="displayName">
-                        <div className="displayPic">
-                            <img src="https://i.pinimg.com/originals/be/ac/96/beac96b8e13d2198fd4bb1d5ef56cdcf.jpg" alt="" />
-                        </div>
-                        <div style={{ display:'flex', flex:'1', justifyContent:'space-between', margin: '0 10px'}}>
-                            <span style={{fontWeight: 500}}>{user.firstName} {user.lastName}</span>
-                            <span>{user.isOnline ? 'online' : 'offline'}</span>
-                        </div>
-                    </div>
+                    <User 
+                      onClick={initChat}
+                      key={user.uid} 
+                      user={user} 
+                    />
                   );
               }) : null
           }   
       </div>
+
       <div className="chatArea">
-          <div className="chatHeader"> Usuário 1 </div>
+
+          <div className="chatHeader">  
+            {
+                chatStarted ? chatUser : ''    
+            }
+          </div>
+
           <div className="messageSections">
-
-              <div style={{ textAlign: 'left' }}>
-                  <p className="messageStyle" >Hello User</p>
-              </div>
-
+              {
+                  chatStarted ?
+                  <div style={{ textAlign: 'left' }}>
+                    <p className="messageStyle" >Hello User</p>
+                  </div> : null
+              }
           </div>
-          <div className="chatControls">
-              <textarea />
-              <button>Send</button>
-          </div>
+          
+          {
+              chatStarted ?
+              <div className="chatControls">
+                <textarea />
+                <button>Send</button>
+              </div> : null
+          }
+
       </div>
   </section>
   </Layout>

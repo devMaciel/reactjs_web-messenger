ede79ece0733699021caa0a36d5d5ce5c81f5883
João Pedro Maciel <joomaciiel2@outlook.com>
2020-10-22 21:33:22 -0300

    chatbox 13-15

--- zapzap/src/Pages/ChatBox/chatbox.js
+++ zapzap/src/Pages/ChatBox/chatbox.js
@@ -28,18 +28,142 @@ export default class ChatBox extends React.Component{
         
         this.stateChanged = localStorage.getItem(LoginString.UPLOAD_CHANGED);
         this.currentPeerUser = this.props.currentPeerUser;
+        this.groupChatId = null;
+        this.listMessage = [];
+        this.currentPeerUserMessages = [];
+        this.removeListener = null;
+        this.currentPhotoFile = null;
 
+        firebase.firestore()
+        .collection('users')
+        .doc(this.currentPeerUser.documentKey)
+        .get()
+        .then((docRef) => {
+            this.currentPeerUserMessages = docRef.data().messages
+        })
+        
+
+    }
+
+    componentDidUpdate(){
+        this.scrollToBottom()
     }
 
     componentWillReceiveProps(newProps){
         if(newProps.currentPeerUser){
-            this.currentUserPhoto = newProps.currentPeerUser;
+            this.currentPeerUser = newProps.currentPeerUser;
             // this.getListHistory();
         }
     }
 
     componentDidMount(){
-        // this.getListHistory();
+        this.getListHistory();
+    }
+
+    getListHistory = () => {
+        this.listMessage.length = 0
+        this.setState({
+            isLoading: true
+        })
+
+        if(
+            this.hashString(this.currentUserId) <=
+            this.hashString(this.currentPeerUser.id)
+        ){
+            this.groupChatId = `${this.currentUserId}-${this.currentPeerUser.id}`
+        }else{
+            this.groupChatId = `${this.currentPeerUser.id}-${this.currentUserId}`
+        }
+
+        //Get history and listen new data added
+        this.removeListener = firebase.firestore()
+        .collection('Messages')
+        .doc(this.groupChatId)
+        .collection(this.groupChatId)
+        .onSnapshot(Snapshot => {
+            Snapshot.docChanges().forEach(change => {
+                if(change.type === LoginString.DOC){ //addeed
+                    this.listMessage.push(change.doc.data())
+                }
+            })
+            this.setState({
+                isLoading: false
+            })
+        }), //watch here
+        err => {
+            this.props.showToast(0, err.toString())
+        }
+    }
+
+    onSendMessage = (content, type) => {
+        let notificationMessages = [];
+
+        if(this.state.isShowSticker && type === 2){
+            this.setState({isShowSticker: false})
+        }
+
+        if(content.trim() === ''){
+            return
+        }
+
+        //content,type from args
+        const timestamp = moment().valueOf().toString()
+        const itemMessage = {
+            idFrom: this.currentUserId,
+            idTo: this.currentPeerUser,
+            timestamp: timestamp,
+            content: content.trim(),
+            type: type
+        }
+
+        firebase.firestore()
+        .collection('Messages')
+        .doc(this.groupChatId)
+        .collection(this.groupChatId)
+        .doc(timestamp)
+        .set(itemMessage)
+        .then(() =>{
+            this.setState({
+                inputValue: ''
+            })
+        })
+
+        this.currentPeerUserMessages.map((item) => {
+            if(item.notificationId != this.currentUserId){
+                notificationMessages.push({
+                    notificationId: item.notificationId,
+                    number: item.number //number of messages received
+                })
+            }
+        })
+
+        firebase.firestore()
+        .collection('users')
+        .doc(this.currentPeerUser.documentKey)
+        .update({
+            messages: notificationMessages
+        })
+        .then((data) => {})
+        .catch(err => {
+            this.props.showToast(0, err.toString());
+        })
+    }
+
+
+    scrollToBottom = () => {
+        if(this.messagesEnd){
+            this.messagesEnd.scrollIntoView({})
+        }
+    }
+
+    onKeyPress = (event) => {
+        if(event.key === 'Enter'){
+            this.onSendMessage(this.state.inputValue, 0);
+        }
+    }
+
+    openListSticker = () => {
+        this.setState({isShowSticker: !this.state.isShowSticker})
     }
 
     render(){
@@ -62,7 +186,7 @@ export default class ChatBox extends React.Component{
                 </div>
 
                 <div className="viewListContentChat">
-                    {/* {} */}
+                    {/* { this.renderListMessage() } */}
                     <div 
                         style={{ float: 'left', clear:'both' }}
                         ref={el => {
@@ -113,9 +237,41 @@ export default class ChatBox extends React.Component{
                     />
 
                 </div>
+
+                {
+                    this.state.isLoading ? (
+                        <div className="viewLoading">
+                            <ReactLoading 
+                                type={'spin'}
+                                color={'#203152'}
+                                height={'3%'}
+                                width={'3%'}
+                            />
+                        </div>
+                    ) : null
+                }
+
             </Card>
         )
     }
+
+
+    // renderListMessage = () => {
+    //     if(this.listMessage.length > 0){
+    //         let viewListMessage = []
+    //         this.listMessage.forEach((item, index) => {
+    //             if(item.idFrom === this.currentUserId){
+    //                 if(item.type === 0){ // 0 is text message
+    //                     viewListMessage.push(
+    //                         <div className="viewItemRight" key={item.timestamp}>
+    //                             <span className="textContentItem">{item.content}</span>
+    //                         </div>
+    //                     )
+    //                 }
+    //             }
+    //         })
+    //     }
+    // }
 
     renderSticker = () => {
         return(
@@ -128,5 +284,16 @@ export default class ChatBox extends React.Component{
                 />
             </div>
         )
+    }
+
+    //for groupchatId, firebase doc setup
+    hashString = (str) => {
+        let hash = 0
+        for(let i = 0; i < str.length; i++){
+            hash += Math.pow(str.charCodeAt(i) * 31, str.length - i)
+            hash = hash & hash // Convert to 32bit integer
+        }
+
+        return hash
     }
 }
